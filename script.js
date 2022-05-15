4ac4f39c5733d49be7bea15316ceb5066c30b0ef
Igor Dolgov <igordolgov1972@gmail.com>
2022-05-15 23:36:08 +0300

    deploy

---
+++ script.js
@@ -0,0 +1,17 @@
+$(".lift").offset({top: 432});
+
+$(".fifth").click(function() {
+    $(".lift").offset({top: 32});
+})
+$(".fourth").click(function() {
+    $(".lift").offset({top: 132});
+})
+$(".third").click(function() {
+    $(".lift").offset({top: 232});
+})
+$(".second").click(function() {
+    $(".lift").offset({top: 332});
+})
+$(".first").click(function() {
+    $(".lift").offset({top: 432});
+})
